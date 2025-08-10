15437cdbfbc9e3a6d2ffc90d83f88c27a45ad914
bauvdel <bauvdel@gmail.com>
2025-08-10 00:07:12 -0400

    Added sync for breadcrumb exports

--- scripts/4_World/UGBreadcrumbObject.c
+++ scripts/4_World/UGBreadcrumbObject.c
@@ -1,7 +1,7 @@
 class UGBreadcrumb : Building
 {
     protected float m_UG_EyeAccommodation = 1.0; // 0..1
-    protected int   m_UG_UseRaycast = 0;         // 0/1
+    protected int   m_UG_UseRaycast = 1;         // 0/1
     protected float m_UG_Radius = -1.0;          // -1 = default
     protected ref Timer m_BCWatch;
     protected vector    m_LastPos;
@@ -16,7 +16,7 @@ class UGBreadcrumb : Building
     {
         if (m_BCWatch) m_BCWatch.Stop();
     }
-    
+
     void BC_Poll()
     {
     vector p = GetPosition();
@@ -32,12 +32,21 @@ class UGBreadcrumb : Building
         if (v < 0.0) v = 0.0;
         if (v > 1.0) v = 1.0;
         m_UG_EyeAccommodation = v;
+        UG_RescanTriggersAround(GetPosition(), 200.0);
     }
     float GetEyeAccommodation() { return m_UG_EyeAccommodation; }
 
-    void SetUseRaycast(int v) { m_UG_UseRaycast = v != 0; }
+    void SetUseRaycast(int v) 
+    { 
+        m_UG_UseRaycast = v != 0;
+        UG_RescanTriggersAround(GetPosition(), 200.0); 
+    }
     int  GetUseRaycast() { return m_UG_UseRaycast; }
 
-    void SetRadius(float r) { m_UG_Radius = r; }
+    void SetRadius(float r) 
+    { 
+        m_UG_Radius = r;
+        UG_RescanTriggersAround(GetPosition(), 200.0);
+    }
     float GetRadius() { return m_UG_Radius; }
 };
